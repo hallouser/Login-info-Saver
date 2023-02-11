2c1ca4c1d1729d56eb345b470d1379a62b6c7099
lumauser <91039519+lumauser@users.noreply.github.com>
2023-02-11 15:59:01 -0800

    full change

---
+++ main.py
@@ -0,0 +1,16 @@
+f = open("info.txt", "w")
+
+app = input("What is the app you want to save info for?: ")
+username = input("What is your username for the app your using?: ")
+password = input("What is your password for the app your using?: ")
+
+f = open("info.txt", "w")
+f.write(app)
+f.write("\nusername:")
+f.write(username)
+f.write("\npassword:")
+f.write(password)
+f.close()
+
+f = open("info.txt", "r")
+print(f.read())
